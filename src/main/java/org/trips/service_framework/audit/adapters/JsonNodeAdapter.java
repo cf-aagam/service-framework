afc0599d91b589da09870807838ab4234f70940b
Anomitra <anomitra.saha@captainfresh.in>
2024-09-06 14:45:07 +0530

    FIX: Changed JsonNodeAdapter

--- src/main/java/org/trips/service_framework/audit/adapters/JsonNodeAdapter.java
+++ src/main/java/org/trips/service_framework/audit/adapters/JsonNodeAdapter.java
@@ -3,6 +3,7 @@ package org.trips.service_framework.audit.adapters;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
@@ -18,7 +19,11 @@ import java.util.List;
  */
 
 @Component
-public class JsonNodeAdapter implements JsonTypeAdapter<JsonNode> {
+public class JsonNodeAdapter implements JsonTypeAdapter<ObjectNode> {
+
+    // NOTE: This class is an adapter for JsonNode objects defined in entities - however
+    // it is written with ObjectNode as the type parameter because JsonNode is an
+    // abstract class, and our JsonNode fields actually contain ObjectNode objects.
 
     private final ObjectMapper objectMapper;
 
@@ -27,25 +32,22 @@ public class JsonNodeAdapter implements JsonTypeAdapter<JsonNode> {
     }
 
     @Override
-    public JsonNode fromJson(JsonElement json,
+    public ObjectNode fromJson(JsonElement json,
                              JsonDeserializationContext jsonDeserializationContext) {
         try {
-            JsonNode jsonNode = objectMapper.readTree(json.toString());
-            return objectMapper.treeToValue(jsonNode, JsonNode.class);
+            return (ObjectNode) objectMapper.readTree(json.toString());
         } catch (JsonProcessingException e) {
             throw new RuntimeException("Failed to deserialize JSON to POJO", e);
         }
     }
 
     @Override
-    public JsonElement toJson(JsonNode sourceValue,
-                              JsonSerializationContext jsonSerializationContext) {
-        JsonNode jsonNode = objectMapper.valueToTree(sourceValue);
-        return JsonParser.parseString(jsonNode.toString());
+    public JsonElement toJson(ObjectNode sourceValue, JsonSerializationContext jsonSerializationContext) {
+        return JsonParser.parseString(sourceValue.toString());
     }
 
     @Override
     public List<Class> getValueTypes() {
-        return List.of(JSONObject.class);
+        return List.of(ObjectNode.class);
     }
 }
